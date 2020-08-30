ccfa9225bfe86783b14bf4271f4fba66cd610767
Taylor Holloway <taylorholloway1984@gmail.com>
2020-08-30 06:35:40 -0500

    adsfasdf

---
+++ app.js
@@ -0,0 +1,34 @@
+//jshint esversion:6
+
+
+const sslRedirect = require('heroku-ssl-redirect');
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
+
+
+const app = express();
+
+
+// app.use(sslRedirect());
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/vendor"));
+app.use(express.static(__dirname + "/.well-known"));
+app.use(express.static(__dirname + "/acme-challenge"));
+
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+//if statement allows running locally as well as on keroku
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started on port 3000");
+});
